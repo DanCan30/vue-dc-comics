c4be02a0d3d2d26fe17f4a6afe5954fe2cd1d639
DanCan30 <danielecan3008@gmail.com>
2022-07-15 14:30:57 +0200

    Scaffolding and header layout done

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Content from './components/Content.vue'
+import Footer from './components/Footer.vue'
+import Socials from './components/Socials.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Content,
+    Footer,
+    Socials,
+
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+  @import "./stiles/common.scss";
+  @import "./stiles/mixins.scss";
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;500;700;900&display=swap');
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <header>
+        <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        <nav>
+
+            <ul>
+                <li v-for="(link, index) in navbarLinks" :key="index">
+                    <a :href="link.href">
+                        {{link.link}}
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+
+            navbarLinks: [
+                {
+                link: "characters",
+                href: "#"
+                },
+                {
+                link: "comics",
+                href: "#"
+                },
+                {
+                link: "movies",
+                href: "#"
+                },
+                {
+                link: "tv",
+                href: "#"
+                },
+                {
+                link: "games",
+                href: "#"
+                },
+                {
+                link: "collectibles",
+                href: "#"
+                },
+                {
+                link: "videos",
+                href: "#"
+                },
+                {
+                link: "fans",
+                href: "#"
+                },
+                {
+                link: "news",
+                href: "#"
+                },
+                {
+                link: "shop",
+                href: "#"
+                },
+            ]
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../stiles/mixins.scss";
+  @import "../stiles/variables.scss";
+
+
+    header {
+        @include generalContainer;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        img {
+            padding: 2rem 0;
+        }
+    }
+    nav {
+        
+        ul {
+            list-style: none;
+
+            li {
+                display: inline-block;
+                
+                a {
+                    color: $textColor;
+                    font-weight: bold;
+                    text-decoration: none;
+                    text-transform: uppercase;
+                    padding: 3rem 1rem;
+
+                    &:hover {
+                        border-bottom: 5px solid $brandColor;
+                        color: $brandColor;
+                    }
+                }
+            }
+        }
+    }
+        
+
+
+</style>
